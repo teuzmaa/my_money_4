6a0b342803abcf4cd4a564343e784011058922bb
teuzmaa <matheuszin1992@hotmail.com>
2023-09-01 21:24:16 -0300

    4 commit

---
+++ lib/src/router/app_router.dart
@@ -0,0 +1,6 @@
+class AppRouter {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String personalRegister = '/personal-register';
+  static const String home = '/home';
+}
